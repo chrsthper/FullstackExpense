58b96b10373cddb6ca09e615544932feee8c8542
ahmadfauzanrinaufaldi <rinaufaldi@gmail.com>
2025-06-05 09:02:03 +0700

    Configuration npm lint  Final

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,37 +4,37 @@ import json from "@eslint/json";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { 
-    files: ["**/*.{js,mjs,cjs}"], 
-    plugins: { js }, 
-    extends: ["js/recommended"] 
-  },
-  { 
-    files: ["**/*.{js,mjs,cjs}"], 
-    languageOptions: { globals: { ...globals.browser, ...globals.node } } 
-  },
-  { 
-    files: ["**/*.json"], 
-    plugins: { json }, 
-    language: "json/json", 
-    extends: ["json/recommended"] 
-  },
-  { 
-    files: ["**/*.css"], 
-    plugins: { css }, 
-    language: "css/css", 
-    extends: ["css/recommended"] 
-  },
-  {
-  ignores: ["__tests__/"]
-  },
-
-  // 👇 Tambahkan blok override ini di paling bawah
-  {
-    files: ["package-lock.json"],
-    rules: {
-      "json/no-empty-keys": "off"
+export default defineConfig(
+  [
+    {
+      files: ["**/*.{js,mjs,cjs}"],
+      plugins: { js },
+      extends: ["js/recommended"]
+    },
+    {
+      files: ["**/*.{js,mjs,cjs}"],
+      languageOptions: { globals: { ...globals.browser, ...globals.node } }
+    },
+    {
+      files: ["**/*.json"],
+      plugins: { json },
+      language: "json/json",
+      extends: ["json/recommended"]
+    },
+    {
+      files: ["**/*.css"],
+      plugins: { css },
+      language: "css/css",
+      extends: ["css/recommended"]
+    },
+    {
+      files: ["package-lock.json"],
+      rules: {
+        "json/no-empty-keys": "off"
+      }
     }
+  ],
+  {
+    ignores: ["__tests__/"]  // ✅ penempatan yang benar
   }
-]);
+);
